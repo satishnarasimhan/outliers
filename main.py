2094281d75d11f7a3925759e8dc19c9404c9a4a7
satishnarasimhan <satishn.narasimhan@gmail.com>
2021-12-10 13:36:19 +0530

    updated files

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ date = day.strftime("%d%m%y")
 #date = '170621'
 print(date)
 
-column_names = ["CONTRACT_D","PREVIOUS_S","OPEN_PRICE","HIGH_PRICE","LOW_PRICE","CLOSE_PRIC","SETTLEMENT","NET_CHANGE","OI_NO_CON", "TRADED_QUA", "TRD_NO_CON", "UNDRLNG_ST", "NOTIONAL_V", "PREMIUM_TR", "SECURITY", "CEPE", "TEMP", "SERIES", "STRIKE", "OPT_CONTRACT", "DATE"]
+column_names = []
 
-sourcePath = 'C:\\Users\\Nithya\\Documents\\Python\\Projects\\Outliers\\Raw Files\\' 
+sourcePath = 'C:\\Users\\Path\\To\\Raw\\Files' 
 #Historical: \\Apr-2021\\ .. Raw Files\\
 fileName = 'op'+date+'.csv'
 csvfile = (sourcePath+fileName) 
@@ -26,14 +26,14 @@ df = pd.DataFrame(columns = column_names)
 df = pd.read_csv(csvfile)
 
 # PE - ITM Selling
-pe_res = df.query("CEPE=='PE' & (STRIKE > UNDRLNG_ST) & OI_NO_CON > 0 & TRD_NO_CON > 0 & HIGH_PRICE < (STRIKE - UNDRLNG_ST) & (SETTLEMENT > 0)")
+pe_res = df.query("")
 
 # Processed file path
-pe_res.to_csv(r'C:\\Users\\Nithya\\Documents\\Python\\Projects\\Outliers\\Processed Files\\PE-ITM'+'_'+date+'.csv')
+pe_res.to_csv(r'')
 print("PE file generated successfully")
 
 # CE -ITM Selling
-ce_res = df.query("CEPE=='CE' & (STRIKE < UNDRLNG_ST) & OI_NO_CON > 0 & TRD_NO_CON > 0 & HIGH_PRICE < (UNDRLNG_ST - STRIKE) & (SETTLEMENT > 0)")
+ce_res = df.query("")
 
-ce_res.to_csv(r'C:\\Users\\Nithya\\Documents\\Python\\Projects\\Outliers\\Processed Files\\CE-ITM'+'_'+date+'.csv')
+ce_res.to_csv(r'')
 print("CE file generated successfully")
